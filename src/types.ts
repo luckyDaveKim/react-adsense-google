5ebc9f5c5da3cd4c13e34933c1179d0d864ec188
luckyDaveKim <lucky_dave@naver.com>
2021-01-07 15:15:48 +0900

    feat: Use typescript

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface IGoogleAdsenseProps {
+  className?: string;
+  style?: object;
+  adClient: string;
+  adSlot: string;
+  adLayout?: string;
+  adLayoutKey?: string;
+  adFormat?: string;
+  fullWidthResponsive?: string;
+}
